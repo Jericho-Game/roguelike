403b3c17d251bda153fe29948c7f9085958543af
anton.kazakov <anton.kazakov@hellotickets.com>
2022-03-03 10:52:31 +0400

    add(api): axios

---
+++ src/utils/axios.ts
@@ -0,0 +1,23 @@
+import axios, {
+  AxiosInstance,
+  AxiosRequestConfig,
+  AxiosError,
+} from 'axios';
+
+const baseConfig: AxiosRequestConfig = {
+  timeout: 3000,
+  withCredentials: true,
+};
+
+type GetAxiosInstance = (baseURL?: string) => AxiosInstance;
+
+const getAxiosInstance: GetAxiosInstance = (baseURL = '/') => (
+  axios.create({
+    ...baseConfig,
+    baseURL,
+  })
+);
+
+const { isAxiosError } = axios;
+export default getAxiosInstance;
+export { AxiosError, isAxiosError };
