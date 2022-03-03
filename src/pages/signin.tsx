c0fd34cb720897a86850ce081b55d34dde9af16e
anton.kazakov <anton.kazakov@hellotickets.com>
2022-03-03 11:14:18 +0400

    add: signin page

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,5 +1,89 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useForm, Controller } from 'react-hook-form';
+import Input from '../components/Input';
+import Button from '../components/Button';
+import Notification from '../components/Notification';
+import authServise from '../services/auth';
+import patterns from '../utils/formValidation';
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ');
+}
+
+type FormData = {
+  login: string;
+  password: string;
+};
+
 export default function SignInPage() {
+  const { control, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const [notification, setNotification] = useState('');
+  const navigate = useNavigate();
+  const onSubmit = handleSubmit((data) => {
+    authServise.signIn(data)
+      // TODO change page
+      .then(() => navigate('/profile'))
+      .catch((error: Error) => {
+        setNotification(error.message);
+      });
+  });
+
+  const notifyNode = notification ? <Notification type="error" className="mt-4 absolute w-[calc(100%-4rem)] bottom-4 ml-8"><span>{notification}</span></Notification> : null;
+
   return (
-    <h1>Sign In Page</h1>
+    <div
+      className={classNames(
+        'grid grid-rows-[1fr] justify-center items-center w-full h-full bg-gray-100',
+      )}
+    >
+      <div
+        className={classNames(
+          'grid grid-rows-[1fr] justify-center items-center bg-white py-6 px-8 relative',
+        )}
+      >
+        <h1 className="font-bold leading-tight  text-4xl text-center mb-6">Sign In Page</h1>
+        <form
+          onSubmit={onSubmit}
+          className={classNames(
+            'rounded-xl  bg-white w-[445px] grid',
+          )}
+        >
+          <Controller
+            name="login"
+            rules={{
+              pattern: {
+                value: patterns.LOGIN,
+                message: 'Login is invalid',
+              },
+              required: 'This is required',
+            }}
+            control={control}
+            render={({ field }) => <Input {...field} id="login" label="Login" errorText={errors.login?.message} />}
+          />
+          <Controller
+            name="password"
+            rules={{
+              pattern: {
+                value: patterns.PASSWORD,
+                message: 'Password is invalid',
+              },
+              required: 'This is required',
+            }}
+            control={control}
+            render={({ field }) => <Input {...field} id="password" label="Password" errorText={errors.password?.message} />}
+          />
+          <Button
+            variant="secondary"
+            className={classNames(
+              'mt-4 mb-16',
+            )}
+          >
+            <span>Отправить</span>
+          </Button>
+        </form>
+        { notifyNode }
+      </div>
+    </div>
   );
 }
