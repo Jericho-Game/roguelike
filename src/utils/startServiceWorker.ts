ee18c65dd51c34e6729611770ce69d92a245dbb8
anton.kazakov <anton.kazakov@hellotickets.com>
2022-04-21 10:45:34 +0500

    fix: startSW

--- src/utils/startServiceWorker.ts
+++ src/utils/startServiceWorker.ts
@@ -1,33 +1,14 @@
-/* eslint-disable */
-
 export default function startServiceWorker() {
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
       navigator.serviceWorker
-        .getRegistrations().then((registrations) => {
-          for (const registration of registrations) {
-            registration.unregister();
-          }
+        .register('/service-worker.js')
+        .then((registration) => {
+          console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        })
+        .catch((error: string) => {
+          console.log('ServiceWorker registration failed: ', error);
         });
-      // .register('/service-worker.js')
-      // .then((registration) => {
-      //   console.log('ServiceWorker registration successful with scope: ', registration.scope);
-      // })
-      // .catch((error: string) => {
-      //   console.log('ServiceWorker registration failed: ', error);
-      // });
     });
   }
-
-  // .then(registrations => {
-  //   registrations.forEach(registration => {
-  //     registration.unregister();
-  //   })
-  // });
-
-  navigator.serviceWorker.getRegistrations().then((registrations) => {
-    for (const registration of registrations) {
-      registration.unregister();
-    }
-  });
 }
