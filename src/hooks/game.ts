d37f7a962b627114402fcff6a41e030c07db0e89
DOXOPOKC <lilondin@gmail.com>
2022-07-13 01:47:16 +0700

    Fullscreen (#12)
    * add: fullscreen

--- src/hooks/game.ts
+++ src/hooks/game.ts
@@ -6,9 +6,18 @@ import GameManager from '../core/gameManager';
 export default () => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [gameManager, setGameManager] = useState<GameManager | null>(null);
+
   const restart = useCallback(() => {
     gameManager?.restart();
   }, [gameManager]);
+
+  const openFullScreen = useCallback(() => {
+    if (!document.fullscreenElement) {
+      canvasRef.current?.requestFullscreen();
+    } else if (document.exitFullscreen) {
+      document.exitFullscreen();
+    }
+  }, []);
 
   useEffect(() => {
     if (canvasRef.current) {
@@ -23,5 +32,5 @@ export default () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { canvasRef, restart };
+  return { canvasRef, restart, openFullScreen };
 };
